f2835a6a8518cdb7ef66946e583263cfcc306a8b
Rakesh <justdecodeme@hotmail.com>
2019-11-12 13:22:35 +0530

    first model baked

---
+++ projects/forum/app/Test/Fixture/TopicFixture.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Topic Fixture
+ */
+class TopicFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 8, 'unsigned' => false, 'key' => 'primary'),
+		'user_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 8, 'unsigned' => false),
+		'title' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 100, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'visible' => array('type' => 'boolean', 'null' => false, 'default' => null, 'comment' => '1 - visible, 2 - hidden'),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'user_id' => 1,
+			'title' => 'Lorem ipsum dolor sit amet',
+			'visible' => 1,
+			'created' => '2019-11-12 08:51:34',
+			'modified' => '2019-11-12 08:51:34'
+		),
+	);
+
+}
